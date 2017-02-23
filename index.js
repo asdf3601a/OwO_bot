4b6806fe154bfc4c8c7409ca3ea74aef35531c54
asdf3601a <asdf3601a@gmail.com>
2017-02-24 01:04:50 +0800

    fix some bug
    fix some bug

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ bot.onText(/\/mcsv (.+)/, (msg, match) => {
       var sv = {}
       if (info.status) {
         sv.status='啟動中'
+        var data = '伺服器名稱:'+info.motd+'\n'+'伺服器狀態:'+sv.status+'\n'+'伺服器位址:'+info.hostname+':'+info.port+'\n'+'版本:'+info.version+'\n'+'線上玩家數/最大玩家數:'+info.players.online+'/'+info.players.max
       } else {
         sv.status='關閉中'
+        var data = '伺服器名稱:'+info.motd+'\n'+'伺服器狀態:'+sv.status+'\n'+'伺服器位址:'+info.hostname+':'+info.port
       }
-      var data = '伺服器名稱:'+info.motd+'\n'+'伺服器狀態:'+sv.status+'\n'+'伺服器位址:'+info.hostname+':'+info.port+'\n'+'版本:'+info.version+'\n'+'線上玩家數/最大玩家數:'+info.players.online+'/'+info.players.max
+
       bot.sendMessage(msg.chat.id,data)
     }
   })
